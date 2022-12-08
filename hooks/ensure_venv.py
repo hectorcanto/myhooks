637165be56f5e2d07ffca7827cda7a447478d7b1
Hector Canto <hector.canto@citnow.com>
2022-12-08 21:25:36 +0100

    use system exit exception

--- hooks/ensure_venv.py
+++ hooks/ensure_venv.py
@@ -5,25 +5,30 @@ from pathlib import Path
 __version__ = "0.1.0"
 
 
-def in_venv():
-    """Check virtualenv active using sys prefixes"""
+def in_venv() ->:
+    """Check virtualenv active using sys prefixes
+
+    Returns:
+        1 if error
+        None if not
+    """
     inside_venv = hasattr(sys, "real_prefix") or (
         hasattr(sys, "base_prefix") and sys.base_prefix != sys.prefix
     )
     if not inside_venv:
         print("You are not in a virtualenv")
-        sys.exit(1)
+        return
     current_venv = sys.prefix.rsplit("/", 1)[-1]
     venv_without_hash = current_venv.split("-",1)[0]
     current_route = Path(__file__).parent.parent.resolve()
 
     if venv_without_hash not in str(current_route):
         print(f"You are not in the right a virtualenv: current: {current_venv}")
-        sys.exit(1)
+        return 1
     print(f"You are in virtualenv '{current_venv}'")
-    sys.exit(0)
+    return
 
 
 if __name__ == "__main__":
-    in_venv()
+    raise SystemExit(in_venv())
 
